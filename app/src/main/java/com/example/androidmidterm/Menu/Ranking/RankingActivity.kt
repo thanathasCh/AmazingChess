2957b0a8d3963d4bd8f3a948e1a9b778ba46906f
tamasama077 <tamasama077@gmail.com>
2019-10-07 02:24:20 +0700

    Query Ranking and remove imageURL

--- app/src/main/java/com/example/androidmidterm/Menu/Ranking/RankingActivity.kt
+++ app/src/main/java/com/example/androidmidterm/Menu/Ranking/RankingActivity.kt
@@ -2,20 +2,52 @@ package com.example.androidmidterm.Menu.Ranking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidmidterm.R
+import com.example.androidmidterm.Services.DbContext
+import com.example.androidmidterm.Services.UserModel
 import com.example.androidmidterm.Services.UserViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_ranking.*
 
 class RankingActivity : AppCompatActivity() {
+
+    val db = DbContext()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
 
-        val a = ArrayList<UserViewModel>()
+        val data = db.Users.orderByChild("Score").limitToLast(10)
 
-        rvRanking.layoutManager = LinearLayoutManager(this)
-        rvRanking.adapter = RankingAdapter(this, a)
+        data.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val top10 = arrayListOf<UserViewModel>()
+
+                dataSnapshot.children.forEach {
+                    top10.add(UserModel(
+                        Id = it.key.toString(),
+                        Name = it.child("Name").value.toString(),
+                        FirstName = it.child("FirstName").value.toString(),
+                        LastName =  it.child("LastName").value.toString(),
+                        Score = it.child("Score").value.toString().toInt(),
+                        CreatedAt = it.child("CreatedAt").value.toString(),
+                        EditedAt = it.child("EditedAt").value.toString()
+                    ).toViewModel())
+                }
+                top10.sortByDescending { it.Score }
+                Log.d("Data", top10.toString())
+                rvRanking.layoutManager = LinearLayoutManager(this@RankingActivity)
+                rvRanking.adapter = RankingAdapter(this@RankingActivity, top10)
+            }
+
+        })
     }
 }
