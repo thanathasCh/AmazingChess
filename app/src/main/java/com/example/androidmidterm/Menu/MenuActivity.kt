2725db55f47fbd10d5e3c5a72d6ef72c560b2480
James S <ps13james@gmail.com>
2019-10-11 13:19:11 +0700

    Complement Music

--- app/src/main/java/com/example/androidmidterm/Menu/MenuActivity.kt
+++ app/src/main/java/com/example/androidmidterm/Menu/MenuActivity.kt
@@ -1,17 +1,23 @@
 package com.example.androidmidterm.Menu
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.androidmidterm.Menu.Ranking.RankingActivity
 import com.example.androidmidterm.R
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
+    private lateinit var mp: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        playMusic()
 
         tvCreateRoom.setOnClickListener {
             val createRoom = Intent(this, CreateRoomActivity::class.java)
@@ -26,6 +32,24 @@ class MenuActivity : AppCompatActivity() {
         tvRanking.setOnClickListener {
             val rank = Intent(this, RankingActivity::class.java)
             startActivity(rank)
+        }
+
+    }
+
+    fun playMusic(){
+        mp = MediaPlayer.create(this, R.raw.hail_to_the_king)
+        mp.start()
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
+
+        ivPlay.setOnClickListener {
+            if (mp.isPlaying) {
+                mp.pause()
+                ivPlay.setBackgroundResource(R.drawable.ic_action_stop)
+            } else {
+                mp.start()
+                ivPlay.setBackgroundResource(R.drawable.ic_action_play)
+            }
         }
     }
 }
